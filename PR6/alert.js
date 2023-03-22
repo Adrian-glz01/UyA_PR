e863ff99b63c9fdb29ef1ff09999326be3414fdd
Adrian-glz01 <alu0101404813@ull.edu.es>
2023-03-22 23:10:19 +0100

    push

---
+++ PR6/alert.js
@@ -0,0 +1,23 @@
+function alert__() {
+  const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+  
+  const alert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('')
+  
+    alertPlaceholder.innerHTML = ''
+    alertPlaceholder.append(wrapper)
+  }
+  
+  const alertTrigger = document.getElementById('liveAlertBtn')
+  if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+      alert('HTML no es un lenguaje de programacion, es un lenguaje de etiquetas', 'success')
+    })
+  }
+}
